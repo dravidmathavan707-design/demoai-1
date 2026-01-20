4e0e09e4cf7824500c1eb1004ea679b154dbe9be
dravidmathavan707 <dravidmathavan707@gmail.com>
2026-01-21 00:13:39 +0530

    new api

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-GEMINI_API_KEY = "AIzaSyBQDXPTukdFtVKrz6czAmogM8i3mQHvkto"
+GEMINI_API_KEY = "AIzaSyADYZSFJqV4IL1r-hhyOOatrPUk478bQVM"
 GEMINI_MODEL = "models/gemini-flash-latest"
 MEMORY_FILE = "data/memory.json"
